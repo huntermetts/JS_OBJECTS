b08dc54480f436d4e2ec6214184d3e4d620c0a5d
Hunter Metts <huntermettsdev@gmail.com>
2018-11-22 11:00:55 -0600

    Connected files, completed first practice exercise in Book 3, Chapter 2

---
+++ main.js
@@ -0,0 +1,9 @@
+const myPet = {
+    name: "Rosie",
+    species:"Mini Australian Shepherd",
+    nicknames:["Rose","Rosielita", "The best pooch ever"],
+    age: 14
+}
+
+console.log(myPet);
+console.log("Hey, I work!");
